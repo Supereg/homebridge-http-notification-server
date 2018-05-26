78b03ffdaeeb4bbe935cbfa19905d3caa5960124
Supereg <mail@anderl-bauer.de>
2018-05-26 17:16:00 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,155 @@
+# homebridge-http-notification-server
+
+`homebridge-http-notification-server` can be used together with [Homebridge](https://github.com/nfarina/homebridge) 
+http accessories. Http accessories are Homebridge plugins, which forward HomeKit requests to another program over a 
+http request. An example for such an accessory would be my 
+[homebridge-http-switch](https://github.com/Supereg/homebridge-http-switch).
+
+The problem with such accessories is when 
+the state of the external program changes it cannot be directly reflected in HomeKit. So one solution would be that 
+every http accessory packs it's own http server to receive state changes. But with multiple switches this becomes a mess 
+very fast.
+
+This is where the `homebridge-http-notification-server` comes in. It is basically a Homebridge plugin, which is loaded by 
+Homebridge like any other plugin but doesn't register any accessories or platforms. Instead it starts ONE http or https 
+server. Http accessories can register with an unique id. Any request the external program will send to the notification 
+server will be forwarded to the accessory which specified the respective `notificationID`.
+
+## Installation
+
+`sudo npm install -g homebridge-http-notification-server`
+
+## Configuration
+
+The configuration file is located in the homebridge directory and needs to be called `notification-server.json`
+
+Example:
+```json
+{
+    "hostname": "127.0.0.1",
+    "port": 8080,
+
+    "ssl": {
+        "privateKey": "/path/to/private-key.prm",
+        "certificate": "/path/to/certificate.cert"
+    }
+}
+```
+
+* `hostname` is optional, default value is `0.0.0.0`
+* `port` is required, default value is `8080`
+* `ssl` is optional. When specified notification-server will create an https server with the specified `privateKey` and 
+`certificate`. Otherwise a default unsecured http server is started.
+
+## How to implement 'homebridge-http-notification-server' into your project
+
+### Implementation in the homebridge accessory (receiver)
+
+First of all you need to specify a handler function in your homebridge accessory. `homebridge-http-notification-server` 
+locates it's registration function in the `notificationRegistration` property of the homebridge api at start.
+
+In order to be sure, that `homebridge-http-notification-server` was already loaded by homebridge, you listen on the event 
+`didFinishLaunching` of the homebridge api.
+
+`notificationRegistration(notificationId, handlerFunction[, password])`
+notificationRegistration function has three parameters, the first two are required.
+* `notificationId`: this is id needs to be unique per homebridge instance. It is later used to identify the accessory when 
+a request is made to the notification-server
+* `handlerFunction`: function which is called when the notification-server received a request for the specified `notificationId`.
+It needs to have one parameter, which is the json body from the http request.
+* `password`: this parameter is fully optional. If specified every request to the notification-server must be authenticated 
+with the specified password. Later more on how a request is constructed.
+
+
+Example http accessory:
+```javascript
+let api;
+
+module.exports = function (homebridgeAPI) {
+    api = homebridgeAPI;
+
+    homebridgeAPI.registerAccessory("homebridge-http-example-accessory", "HTTP-ACCESSORY", HTTP_ACCESSORY);
+};
+
+function HTTP_ACCESSORY(log, config) {
+    // Some initialization
+    this.name = config.name;
+    
+    this.service = new Service.Switch(this.name);
+    this.service.getCharacteristic(Characteristic.On)
+            .on("get", this.getStatus.bind(this))
+            .on("set", this.setStatus.bind(this));
+
+    api.on('didFinishLaunching', function() {
+        // check if notificationRegistration is set, if not 'notificationRegistration' is probably not installed on the system
+        if (api.notificationRegistration && typeof api.notificationRegistration === "function") {
+            api.notificationRegistration("accessory-identifier", this.handleNotification.bind(this), "top-secret-password");
+        }
+    }.bind(this));
+}
+
+HTTP_ACCESSORY.prototype = {
+    
+    identify: function (callback) {
+        this.log("Identify requested!");
+        callback();
+    },
+
+    getServices: function () {
+        return [this.service];
+    },
+    
+    handleNotification: function (jsonRequest) {
+        const service = jsonRequest.service; // value is optional and only relevant if your accessory exposes multiple services
+        
+        const characteristic = jsonRequest.characteristic;
+        const value = jsonRequest.value;
+        
+        let characteristicType;
+        switch (characteristic) {
+            case "On":
+                characteristicType = Characteristic.On;
+                break;
+            default:
+                this.log("Encountered unknown characteristic when handling notification: " + jsonRequest.characteristic);
+                return;
+        }
+
+        this.service.setCharacteristic(characteristicType, value);
+    },
+    
+    getStatus: function(callback) {
+      // http request
+    },
+    
+    setStatus: function(on, callback) {
+      // http request
+    }
+    
+};
+```
+
+### Implementation in the http application (sender)
+
+The http application sends a request to the notification-server (inside of homebridge) to update a value of a HomeKit 
+characteristic. The http request must be a `POST` request. The url would be constructed as follows:
+
+`http://<hostname>:<port>/<notificationID>?password=<password>` (`https://...` if ssl is turned on; if no password was set 
+you don't specify the `password` parameter)
+
+In our example the url would look like the following:
+`http://127.0.0.1:8080/accessory-identifier?password=top-secret-password`
+
+The POST body would look like the following:
+```json
+{
+    "service": "switch-service",
+    "characteristic": "On",
+    "value": true
+}
+```
+* `service` is fully optional. It is only useful if your accessory exposes multiple services. But if you specify it, it 
+must be a string.
+* `characteristic` is required. It represents the name of the characteristic which is going to be updated. Value must be 
+a string. Of course this only works with characteristics which have notify permissions in the HAP specifications. 
+* `value` is required. 
---
+++ notification-server.js
@@ -0,0 +1,294 @@
+const fs = require("fs");
+const path = require('path');
+const url = require("url");
+const http = require("http");
+const https = require("https");
+
+const handlers = {};
+
+module.exports = function(homebridgeApi) {
+    homebridgeApi.notificationRegistration = notificationRegistration.bind(this);
+
+    main(homebridgeApi);
+};
+
+/**
+ * API call which is used by an accessory to listen for incoming update requests
+ *
+ * @param notificationID - internalID the accessory is referencing itself and which is included in the http request to identify the accessory
+ * @param handlerFunction - function, which has one argument for the json body
+ * @param password - <optional> if specified every http request to the notification-server needs to be authenticated with the accessory specific password
+ */
+function notificationRegistration(notificationID, handlerFunction, password) {
+    log("'" + notificationID + "' registered for notifications " + (password?"(authenticated)":"(without authentication)"));
+
+    handlers[notificationID] = {
+        handler: handlerFunction,
+        password: password
+    };
+}
+
+/**
+ * main method to start the notification server
+ *
+ * @param api - HomeBridge API
+ */
+function main(api) {
+    log("Initializing Notification Server...");
+
+    const storagePath = api.user.storagePath();
+    const configPath = path.join(storagePath, "notification-server.json");
+
+    let configuration = { //default configuration
+        port: 8080
+    };
+
+    if (!fs.existsSync(configPath)) {
+        log("Could not find config for notification-server (" + configPath + "). Using default configuration");
+    }
+    else {
+        try {
+            log("Parsing config file...");
+            configuration = JSON.parse(fs.readFileSync(configPath));
+        } catch (error) {
+            error("Could not read 'notification-server.json' config file. Maybe some malformed JSON?");
+            error("Try to validate your JSON with http://jsonlint.com");
+            error("");
+            error(error);
+            error("");
+            error("We continue with using default configuration");
+        }
+    }
+
+    init(configuration);
+}
+
+/**
+ * initializes the http/https server
+ *
+ * @param configuration - content of config file from disk
+ */
+function init(configuration) {
+    let secure = false;
+    const listenOptions = {};
+    let secureOptions = {};
+
+    if (configuration.hostname)
+        listenOptions.host = configuration.hostname;
+
+    listenOptions.port = configuration.port || 8080;
+    if (typeof configuration.port !== "number") {
+        listenOptions.port = 8080;
+        error("The property port in the cofiguration is not a number. Falling back to the default value.");
+    }
+
+    if (configuration.ssl && configuration.ssl.privateKey && configuration.ssl.certificate) {
+        secure = true;
+
+        let key;
+        let cert;
+
+        log("Configuring SSL...");
+        try {
+            key = fs.readFileSync(configuration.ssl.privateKey);
+        } catch (error) {
+            error("Could not read ssl privateKey from disk, falling back to http");
+            error(error);
+
+            secure = false;
+        }
+
+        try {
+            cert = fs.readFileSync(configuration.ssl.certificate);
+        } catch (error) {
+            error("Could not read ssl certificate from disk, falling back to http");
+            error(error);
+
+            secure = false;
+        }
+
+        if (secure) {
+            secureOptions = {
+                key: key,
+                cert: cert
+            }
+        }
+    }
+
+    log("Starting " +(secure? "HTTPS": "HTTP") + " server...");
+    const server = secure? https.createServer(secureOptions, handleHTTPCall): http.createServer(handleHTTPCall);
+
+    server.listen(listenOptions);
+    log("Listening on " + (listenOptions.host? listenOptions.host: "0.0.0.0") + ":" + listenOptions.port);
+}
+
+/**
+ * validates that the body has the required format
+ * developers can add MORE field but not LESS
+ *
+ * body format:
+ * {
+ *      service: "switchOne", // <optional>: data type must be string
+ *      characteristic: "On", // <required>: data type must be string
+ *      value: 56 // <required>
+ * }
+ *
+ * @param body
+ */
+function validateJsonBody(body) {
+    if (typeof body !== "object")
+        throw new Error("Json string is not an object");
+
+    if (!(body.hasOwnProperty("characteristic") || body.hasOwnProperty("value")))
+        throw new Error("Missing required property");
+
+    if (body.hasOwnProperty("service") && typeof body.service !== "string")
+        throw new Error("property 'service' has an invalid data type");
+
+    if (typeof body.characteristic !== "string")
+        throw new Error("property 'characteristic' has an invalid data type");
+}
+
+/**
+ * handles every incoming http call
+ *
+ * @param request
+ * @param response
+ */
+function handleHTTPCall(request, response) {
+    if (request.method !== "POST") {
+        response.writeHead(405, {'Content-Type': "text/html"});
+        response.write("Method Not Allowed");
+        response.end();
+
+        log("Someone tried to access the server without an POST request");
+        return;
+    }
+
+    const parts = url.parse(request.url, true);
+
+    const query = parts.query;
+
+    const pathname = parts.pathname.charAt(0) === "/"
+        ? parts.pathname.substring(1)
+        : parts.pathname;
+    const path = pathname.split("/");
+
+    if (path.length !== 1) {
+        response.writeHead(400, {'Content-Type': "text/html"});
+        response.write("Bad Request");
+        response.end();
+
+        log("Bad Request: " + parts.pathname);
+    }
+    else {
+        const notificationID = path[0];
+        const handlerObject = handlers[notificationID];
+
+        if (handlerObject) {
+            let handler = handlerObject.handler;
+            let password = handlerObject.password;
+
+            if (password && (!query.hasOwnProperty("password") || query.password !== password)) {
+                response.writeHead(401, {'Content-Type': "text/html"});
+                response.write("Unauthorized");
+                response.end();
+
+                log("'" + notificationID + "' tried to get access without authorization");
+                return;
+            }
+
+            let body = "";
+            let invalid = false;
+            request.on("data", function (data) {
+                body += data;
+
+                // Too much POST data, kill the connection!
+                // 1e6 === 1 * Math.pow(10, 6) === 1 * 1000000 ~~~ 1MB
+                if (body.length > 1e6) {
+                    body = "";
+                    invalid = true;
+
+                    error("'" + notificationID + "' sent POST with too large body. Destroying connection...");
+
+                    response.writeHead(413, {'Content-Type': 'text/plain'});
+                    response.write("Payload Too Large");
+                    response.end();
+
+                    request.connection.destroy();
+                }
+            });
+
+            request.on("end", function () {
+                if (invalid)
+                    return;
+
+                response.writeHead(200, {'Content-Type': 'text/html'});
+                response.write("OK");
+                response.end();
+
+                let jsonBody;
+                try {
+                    jsonBody = JSON.parse(body);
+
+                    validateJsonBody(jsonBody);
+                } catch (error) {
+                    response.writeHead(400, {'Content-Type': 'text/html'});
+                    response.write("Bad Request");
+                    response.end();
+
+                    error("'" + notificationID + "' sent malformed body: " + error.message);
+                    return;
+                }
+
+                try {
+                    handler(jsonBody);
+                } catch (error) {
+                    error("Handler encountered error when parsing body:");
+                    error(error);
+
+                    response.writeHead(500, {'Content-Type': 'text/html'});
+                    response.write("Internal Server Error");
+                    response.end();
+                }
+            });
+        }
+        else {
+            response.writeHead(404, {'Content-Type': "text/html"});
+            response.write("Not Found");
+            response.end();
+
+            log("could not find accessory '" + notificationID +"'");
+        }
+    }
+}
+
+/**
+ * replicates basic log format from homebridge logger
+ *
+ * @param message - message to log
+ */
+function log(message) {
+    const prefix = "notification-server";
+    message = "[" + prefix + "] " + message;
+
+    const date = new Date();
+    message = "[" + date.toLocaleString() + "] " + message;
+
+    console.log(message);
+}
+
+/**
+ * replcaites basic error format from homebridge logger
+ *
+ * @param message - message to log to error
+ */
+function error(message) {
+    const prefix = "notification-server";
+    message = "[" + prefix + "] " + message;
+
+    const date = new Date();
+    message = "[" + date.toLocaleString() + "] " + message;
+
+    console.error(message);
+}
